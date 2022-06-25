5deafec3d716efaa68eb81561fb0c2f47e06fbe9
Rahul Kag <kag.rahul@gmail.com>
2022-06-25 18:09:54 +0530

    adding typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const  App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Web3 try
+      </header>
+    </div>
+  );
+}
+
+export default App;
